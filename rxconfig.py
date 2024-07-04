ee8e00d927bdb6b00a1d44a2fd560fa09e39ac6f
wenke.lai <wenke.studio@gmail.com>
2024-07-05 00:13:51 +0800

    fix: add the default config

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,14 @@
 import reflex as rx
+from reflex.constants import LogLevel
+
+# Define the configuration for the Reflex app
+# https://reflex.dev/docs/api-reference/config/
 
 config = rx.Config(
     app_name="reflex_survey",
+    loglevel=LogLevel.DEBUG,
+    api_url="http://localhost:8000",  # The backend url the frontend will connect to.
+    db_url="sqlite:///reflex.db",
+    # redis_url="redis://localhost:6379/0",
+    cors_allowed_origins=["*"],
 )
